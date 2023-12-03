917d1a7c89c080c7e63b68ec2dfef220408178e4
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-03 17:46:51 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+// write a c++ program to print hello on the screen using function...
+#include<iostream>
+using namespace std;
+void f1();
+int main()
+{
+    f1();
+}
+void f1()
+{
+    cout<<"Hello c++";
+}
+
+// o/p---     Hello c++
+ 
+
+     
