0c335007e066c6327be54637954fcc3e924b5105
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-03 19:40:55 +0530

    Add files via upload

---
+++ endl.cpp
@@ -0,0 +1,15 @@
+//write a c++ program to print hello on the first line and MysirG on the second line using endl.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello"<<endl<<"MysirG";
+    return 0;
+}
+
+
+/*
+o/p--   Hello
+        MysirG
+*/
