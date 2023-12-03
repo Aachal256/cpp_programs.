57443d78610f24c3e079a93215bc0fef075f9980
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-03 19:46:13 +0530

    Add files via upload

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+// write a c++ program to calculate the sum of two numbers..
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum="<<c;
+    return 0;
+}
+
+/*
+i/p-  Enter two numbers10 20
+o/p-  sum=30
+*/
