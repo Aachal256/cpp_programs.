d8d968a003e596ed0fcf2e2351de8d5ee4fe50cd
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-05 15:09:57 +0530

    Add files via upload

---
+++ square-of-number-calculate.cpp
@@ -0,0 +1,18 @@
+// write a c++ program to calculate the square of a numbers..
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter a number:";
+    cin>>a;
+    b=a*a;
+    cout<<"square = "<<b;
+    return 0;
+}
+
+/*
+i/p-  Enter a number:6
+o/p-- square = 36
+*/
