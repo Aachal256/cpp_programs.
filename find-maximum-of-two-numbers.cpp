32f6183ec63512f3a8512c6007ee731711d6335c
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-05 15:01:48 +0530

    Add files via upload

---
+++ find-maximum-of-two-numbers.cpp
@@ -0,0 +1,20 @@
+// write a c++ program to find the maximum of two numbers..
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    if(a>b)
+    cout<<a<<" is greater";
+    else
+    cout<<b<<" is greater";
+    return 0;
+}
+
+/*
+i/p-- Enter two numbers:50 20
+o/p-  50 is greater
+*/
