25321472c22a37628865f749a649a3159ddcebb8
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-17 17:25:57 +0530

    Add files via upload

---
+++ even-odd-number.cpp
@@ -0,0 +1,19 @@
+// Even or odd number print.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Even number";
+    else
+    cout<<"odd number";
+    return 0;
+}
+
+/*
+i/p--   Enter a number : 7
+o/p-    odd number
+*/
