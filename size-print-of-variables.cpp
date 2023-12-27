80f9df81176c2d31b62bc886e10fadcedc249dc2
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2023-12-27 23:41:32 +0530

    Add files via upload

---
+++ size-print-of-variables.cpp
@@ -0,0 +1,20 @@
+//WAP to  print size of an int ,float,char type variable.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    cout<<"size of int="<<sizeof(a)<<endl;
+    cout<<"size of float="<<sizeof(b)<<endl;
+    cout<<"size of char="<<sizeof(c);
+    return 0;
+
+}
+
+/*
+i/p---  size of int=4
+        size of float=4
+         size of char=1
+*/
