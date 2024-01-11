f39abef55711b688cbedd5c23951e87ef0f3e027
Aachal Thaware <129490535+Aachal256@users.noreply.github.com>
2024-01-11 23:24:08 +0530

    Add files via upload

---
+++ pattern_right_half_pyramid.cpp
@@ -0,0 +1,28 @@
+/* Right Half pyramid.
+
+ *       
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  *
+ *  *  *  *  *  *
+ *  *  *  *  *  *  *
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<=7;i++)
+    {
+        for(j=1;j<=7;j++)
+        if(j<=i)
+        cout<<" * ";
+        else
+        cout<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
